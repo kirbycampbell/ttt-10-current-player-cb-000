7a58447225931fe98910054d3f3bfdd0ff9a6463
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-14 04:02:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,13 +4,13 @@ def turn_count (board)
     if token == "X" || token == "O"
       counter += 1
    end
-  return counter
+  counter
 end
 
 def current_player(board)
   if turn_count(board) % 2 == 0
-    return "X"
+    "X"
   else
-    return "O"
+    "O"
     end
 end
